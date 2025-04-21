6cf5bc7f54831a0059a541ddd00e06d9530753ba
sajal-mazumder <sajalmazumder47@gmail.com>
2025-04-21 20:27:51 +0600

    some update

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-function name(params) {
-    document.getElementById().classList.remove()
+function showSection(params) {
+   var w = document.getElementById(params + 'Section').classList.remove('hiden') 
 }
+showSection()
+// console.log(w);

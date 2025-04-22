b127a59ae8cbfc2d6d874dee56bb75c734ed14fe
sajal-mazumder <sajalmazumder47@gmail.com>
2025-04-22 23:45:05 +0600

    some update

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function showSection(params) {
+
+   const elements = document.querySelectorAll(".main_content > div")
+
+   elements.forEach(data =>{
+      data.classList.add('hiden')
+   })
+
    var w = document.getElementById(params + 'Section').classList.remove('hiden') 
 }
 showSection()
